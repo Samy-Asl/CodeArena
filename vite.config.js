ff4051ff012f56fbe697c84f93e5c67bf0584135
Samy <samy.asselah@esst-sup.com>
2026-04-28 20:46:02 +0100

    setup deploy github pages

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
+  base: "/CodeArena/",
   plugins: [react()],
   optimizeDeps: {
     include: ['@codemirror/view', '@codemirror/state', '@codemirror/lang-javascript', '@codemirror/lang-python', '@codemirror/theme-one-dark'],
